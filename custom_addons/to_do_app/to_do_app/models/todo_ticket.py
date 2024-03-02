01ba2da91d641afce699be4e5a99ec5d11d3261a
ManarArabi1 <98833607+ManarArabi1@users.noreply.github.com>
2024-03-02 19:19:05 +0200

    lab1

---
+++ custom_addons/to_do_app/to_do_app/models/todo_ticket.py
@@ -0,0 +1,17 @@
+from odoo import models, fields
+
+class TodoTicket(models.Model):
+    _name = 'todo_app.ticket'
+    _description = 'Todo Ticket'
+
+    name = fields.Char(required=True)
+    number = fields.Char(string='Number')
+    tag = fields.Char(string='Tag')
+    state = fields.Selection([
+        ('new', 'New'),
+        ('doing', 'Doing'),
+        ('done', 'Done')
+    ], string='State', default='new')
+    file = fields.Binary(string='File')
+    assign_to = fields.Many2one('res.users', string='Assigned To')
+    description = fields.Text(string='Description')
